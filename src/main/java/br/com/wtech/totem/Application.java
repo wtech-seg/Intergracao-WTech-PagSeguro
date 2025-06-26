d4116339b2fb40d6c78132ad741eafde25e4b950
carol <carolnesso@gmail.com>
2025-06-26 15:03:41 -0300

    comentario com variaveis de sistema do cliente MARROQUIM ARAPIRACA

--- src/main/java/br/com/wtech/totem/Application.java
+++ src/main/java/br/com/wtech/totem/Application.java
@@ -3,6 +3,9 @@ package br.com.wtech.totem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//remember Set ambient variables in the final OS
+//setx PAGSEGURO_EMAIL "suavagaarapiraca@gmail.com"
+//setx PAGSEGURO_TOKEN "B8B9DF4E0BF2479E8676AF43AA2B5FF7"
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
